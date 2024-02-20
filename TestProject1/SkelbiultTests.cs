ef965abcadd97286af84fe73bf659ddabf169777
naglismoc <dvd.filmas@gmail.com>
2024-02-20 16:25:17 +0200

    selenium sample

--- TestProject1/SkelbiultTests.cs
+++ TestProject1/SkelbiultTests.cs
@@ -10,13 +10,23 @@ namespace Selenium
         [SetUp]
         public void Setup()
         {
+            //ChromeOptions options = new ChromeOptions();
+            //options.AddArgument("--headless");
+            //driver = new ChromeDriver(options);
             driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
         }
 
         [Test]
         public void Test1()
         {
             driver.Navigate().GoToUrl("https://skelbiu.lt");
+            driver.FindElement(By.XPath("//*[@id=\"onetrust-accept-btn-handler\"]")).Click();
+            driver.FindElement(By.Id("searchKeyword")).SendKeys("distiliatorius");
+            driver.FindElement(By.Id("searchButton")).Click();
+            string text = driver.FindElement(By.XPath("//*[@id=\"ads68011637\"]/div[2]/div[2]/div[2]")).Text;
+            Console.WriteLine(text);
         }
 
 
